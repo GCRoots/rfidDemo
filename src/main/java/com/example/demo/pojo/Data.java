feadf0b9ba87da6b77ceb23a9bf32d05915c44b4
GC <1007879951@qq.com>
2019-11-04 21:02:23 +0800

    basic

--- src/main/java/com/example/demo/pojo/Data.java
+++ src/main/java/com/example/demo/pojo/Data.java
@@ -6,6 +6,21 @@ package com.example.demo.pojo;
  */
 public class Data {
 
+    //BASIC ATTRIBUTE
+    private String version; // _VERSION 版本号,用于锁
+    private String cuid;    // _CREATE_USER_ID 创建用户ID
+    private String cname;   // _CREATE_USER_NAME 创建用户名
+    private String ctime;   // _CREATE_TIME 创建时间
+    private String uuid;    // _UPDATE_USER_ID 更新用户ID
+    private String uname;   // _UPDATE_USER_NAME 更新用户名
+    private String utime;   // _UPDATE_TIME 更新时间
 
+    private String id; // ID
+    private String name; // NAME 名称(款式、类型、色号、尺码)
+    private String code; // CODE 代码(类型、色号、尺码)
+    private String factoryID; // FACTORY_ID 工厂ID
+    private String goodsTypeID; // GOODS_TYPE_ID 所属商品类型ID
+    private String goodsStyleID; // GOOD_STYLE_ID 所属款号ID
+    private String goodsColorID; // GOODS_COLOR_ID 所属色号ID
 
 }
