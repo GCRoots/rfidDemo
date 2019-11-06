76c4be73f0697b22f62ebb72d21240c110baeec9
GC <1007879951@qq.com>
2019-11-06 17:07:36 +0800

    Mybatis's basic configuration

--- src/main/java/com/example/demo/dao/deemo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.dao;
-
-/**
- * @author shipengfei
- * @data 19-11-4
- */
-public class deemo {
-}
--- src/main/java/com/example/demo/server/imp/TailorInfoServerImp.java
+++ src/main/java/com/example/demo/server/imp/TailorInfoServerImp.java
@@ -1,10 +1,12 @@
 package com.example.demo.server.imp;
 
 import com.example.demo.server.TailorInfoServer;
+import org.springframework.stereotype.Service;
 
 /**
  * @author shipengfei
  * @data 19-11-4
  */
+@Service
 public class TailorInfoServerImp implements TailorInfoServer {
 }
